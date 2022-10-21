3b2875ebd8f88b6f41203234d4eff062406487a6
JamesHsu <aac921oo@gmail.com>
2022-10-21 17:23:29 +0800

    add some new trips on 2013CNY and fixed fittrip owl

--- FitTrip/js/fitTrip_2022.js
+++ FitTrip/js/fitTrip_2022.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
+  // fit_slider
+  $(".fit_slider_banner").owlCarousel({
+    animateOut: "fadeOut",
+    animateIn: "fadeInRight",
+    items: 1,
+    nav: false,
+    loop: true,
+    smartSpeed: 400,
+    autoplay: true,
+    autoplayTimeout: 10000,
+    dots: false,
+  });
+
   // AirTicket
   $(".AirTicket-tab").simpleTab({
     defaultSkin: true,
