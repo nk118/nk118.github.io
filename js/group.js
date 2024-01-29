638772517a92ca8a6dbe6a9b497b3ba10af32877
nk118 <a00922633620@yahoo.com.tw>
2024-01-30 00:20:27 +0800

    新增貼心好禮

--- js/group.js
+++ js/group.js
@@ -68,4 +68,36 @@ $(".responsive1").slick({
     }
   ]
 });
+$(".responsive2").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
 
