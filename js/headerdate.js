23d0d52d93c7852afbe1278498155695b49df567
nk118 <a00922633620@yahoo.com.tw>
2024-02-01 10:33:30 +0800

    更新

--- js/headerdate.js
+++ js/headerdate.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
     // 定义 currentMonth 和 currentYear 变量
-    var currentMonth = new Date().getMonth() ; 
-    var currentYear = new Date().getFullYear();
+    var currentMonth = 0;  // 将初始月份设为0，以确保在第一个月显示有数据的月份
+    var currentYear = 2024; // 设定一个默认年份，你也可以根据实际情况设定
 
     // 初始显示第一筆資料的年月
-    displayMonth(2024, 1); // 假设第一筆資料的年月是2024年1月
+    displayInitialMonth();
 
     // 点击右箭头切换下一个月
     $("#rightArrow").on("click", function () {
@@ -36,31 +36,59 @@ $(document).ready(function () {
 
     // 获取下一个月
     function getNextMonth() {
-        var nextMonth = currentMonth + 1;
-        var nextYear = currentYear;
-
-        if (nextMonth > 12) {
-            nextMonth = 1;
-            nextYear++;
+        for (var nextMonth = currentMonth + 1; ; nextMonth++) {
+            var nextYear = currentYear;
+    
+            if (nextMonth > 12) {
+                nextMonth = 1;
+                nextYear++;
+            }
+    
+            var targetContainer = $(".datemain-container[trip-date='" + nextYear + "-" + nextMonth + "']");
+    
+            if (targetContainer.length > 0) {
+                currentMonth = nextMonth;
+                currentYear = nextYear;
+                displayMonth(currentYear, currentMonth);
+                break;
+            }
+            
         }
-
-        currentMonth = nextMonth;
-        currentYear = nextYear;
-        displayMonth(currentYear, currentMonth);
+    if (targetContainer.length ===0) {
+        displayInitialMonth();
+    }
+      
+     
     }
 
     // 获取上一个月
     function getPrevMonth() {
-        var prevMonth = currentMonth - 1;
-        var prevYear = currentYear;
+        for (var prevMonth = currentMonth - 1; ; prevMonth--) {
+            var prevYear = currentYear;
 
-        if (prevMonth < 1) {
-            prevMonth = 12;
-            prevYear--;
+            if (prevMonth < 1) {
+                prevMonth = 12;
+                prevYear--;
+            }
+
+            if ($(".datemain-container[trip-date='" + prevYear + "-" + prevMonth + "']").length > 0) {
+                currentMonth = prevMonth;
+                currentYear = prevYear;
+                displayMonth(currentYear, currentMonth);
+                break;
+            }
         }
-
-        currentMonth = prevMonth;
-        currentYear = prevYear;
-        displayMonth(currentYear, currentMonth);
     }
+
+    // 初始显示第一筆資料的年月
+    function displayInitialMonth() {
+        // 找到第一笔数据的年月
+        var initialContainer = $(".datemain-container:first");
+        var tripDate = initialContainer.attr("trip-date");
+        var [year, month] = tripDate.split("-").map(Number);
+
+        // 初始显示
+        displayMonth(year, month);
+    }
+    
 });
