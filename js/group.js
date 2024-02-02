7e629c0e5e25b77e8f36c8f2d768022db3b41abf
nk118 <a00922633620@yahoo.com.tw>
2024-02-02 14:34:02 +0800

    20240202

--- js/group.js
+++ js/group.js
@@ -13,8 +13,8 @@ $(".responsive").slick({
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         infinite: true,
         dots: true
       }
